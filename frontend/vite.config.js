e17d718b5aaa944b676cfb656b21638d65e7a566
Jimie165 <149871921+Jimie165@users.noreply.github.com>
2026-06-30 08:57:36 +0800

    fix: 修复5173端口被系统占用的问题

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,8 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 5173,
-    host: '0.0.0.0',
+    port: 5250,
+    host: '127.0.0.1',
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:8080',
